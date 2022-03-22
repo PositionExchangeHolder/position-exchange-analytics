bad3b0c5a94743efb21c8d57819c061d890be807
gafranslotteria <gafranslotteria@gmail.com>
2022-03-22 22:44:44 +0700

    fix style page not found

--- src/components/layout/ContainerLayout.js
+++ src/components/layout/ContainerLayout.js
@@ -1,3 +1,5 @@
 export default function ContainerLayout({ children }) {
-  return <div className="min-h-screen bg-primary px-44 pb-20  ">{children}</div>
+  return (
+    <div className="min-h-screen bg-primary px-44 pb-20 flex ">{children}</div>
+  )
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function PageNotFound() {
   return (
-    <div className="h-screen w-screen bg-gray-100 flex items-center">
-      <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+    <div className="flex  justify-center items-center w-full">
+      <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-500">
         <div className="max-w-md">
           <div className="text-5xl font-dark font-bold">404</div>
           <p className="text-2xl md:text-3xl font-light leading-normal">
