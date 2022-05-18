a38b0f94579c1dbc92853beeda20ef346e9aed98
holder1-cmyk <positionexchange.holder@gmail.com>
2022-05-18 08:54:57 +0700

    refactor: change balance data

--- src/pages/address/[account].tsx
+++ src/pages/address/[account].tsx
@@ -12,7 +12,7 @@ import { isEmpty } from 'lodash'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-import { convertBigNumberToStringNumber } from 'utils/number'
+import { convertBigNumberToNumber } from 'utils/number'
 import { hashFormatter } from 'utils/string'
 
 export default function Account() {
@@ -20,14 +20,17 @@ export default function Account() {
   const account: string = (router?.query?.account as string) || ''
   const [balance, setBalance] = useState<DataBalancerResponse>()
 
-  const totalWallet = +convertBigNumberToStringNumber(
-    balance?.totalPosiBalance?.walletBalance || 1
+  const totalWallet = convertBigNumberToNumber(
+    balance?.totalPosiBalance?.walletBalance
+    || 0
   )
-  const totalPending = +convertBigNumberToStringNumber(
-    balance?.totalPosiBalance?.pendingBalance || 1
+  const totalPending = convertBigNumberToNumber(
+    balance?.totalPosiBalance?.pendingBalance
+    || 0
   )
-  const totalStaking = +convertBigNumberToStringNumber(
-    balance?.totalPosiBalance?.stakingBalance || 1
+  const totalStaking = +convertBigNumberToNumber(
+    balance?.totalPosiBalance?.stakingBalance
+    || 0
   )
   const dataDoughnutWalletChart = transformDataWalletDoughnutChart({
     totalWallet,
@@ -47,10 +50,10 @@ export default function Account() {
   }, [account])
 
   return (
-    <main className="relative px-6 mt-10 w-full  bg-light-primary  dark:bg-primary   md:mt-16  xl:px-0">
+    <main className="relative px-6 mt-10 w-full bg-light-primary dark:bg-primary md:mt-16 xl:px-0">
       <div className="flex justify-center">
-        <div className="flex flex-col  items-center">
-          <div className=" flex justify-center items-center w-32 h-32 rounded-full ring-1 ring-white/5  shadow-md drop-shadow-[0_1px_2px_#1B2431] ">
+        <div className="flex flex-col items-center">
+          <div className=" flex justify-center items-center w-32 h-32 rounded-full ring-1 ring-white/5 shadow-md drop-shadow-[0_1px_2px_#1B2431] ">
             <Image
               alt="avatar"
               width={52}
@@ -60,7 +63,7 @@ export default function Account() {
             />
           </div>
           <span className="mt-6 text-2xl">Kate Horwitz</span>
-          <div className="py-2 px-4 mt-3 bg-primary rounded-[30px] ring-1  ring-white/5     shadow-md drop-shadow-[0_1px_2px_#1B2431]">
+          <div className="py-2 px-4 mt-3 bg-primary rounded-[30px] ring-1 ring-white/5 shadow-md drop-shadow-[0_1px_2px_#1B2431]">
             <span className=" text-sm  ">{hashFormatter(account, true)}</span>
           </div>
           <SocialButton />
@@ -76,7 +79,7 @@ export default function Account() {
       </div>
 
       <div className="grid-cols-3 gap-x-12 mt-12 w-full md:grid">
-        <div className="group flex flex-col col-span-1 gap-y-4 justify-center items-center  h-80 bg-secondary rounded-md">
+        <div className="group flex flex-col col-span-1 gap-y-4 justify-center items-center h-80 bg-secondary rounded-md">
           <div className="w-96 h-96">
             <DoughnutChart
               data={dataDoughnutWalletChart.data}
@@ -84,13 +87,13 @@ export default function Account() {
             />
           </div>
         </div>
-        <div className="flex flex-col col-span-2  justify-between  mt-12 w-full h-80  bg-secondary md:mt-0 ">
+        <div className="flex flex-col col-span-2 justify-between mt-12 w-full h-80 bg-secondary md:mt-0 ">
           <div className="flex flex-row">
-            <div className="flex flex-row items-center  px-6  h-16 text-xs font-medium  md:text-base  ">
+            <div className="flex flex-row items-center px-6 h-16 text-xs font-medium md:text-base">
               <div>PnL: </div>
               <div className="ml-2 text-red-500">{'-$0.88'}</div>
             </div>
-            <div className="flex flex-row items-center  px-6  h-16 text-xs font-medium  md:text-base  ">
+            <div className="flex flex-row items-center px-6 h-16 text-xs font-medium md:text-base">
               <div>All-time ROI: </div>
               <div className="ml-2 text-red-500">{'-11%'}</div>
             </div>
