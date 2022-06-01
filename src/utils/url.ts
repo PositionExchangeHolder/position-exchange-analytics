d285d376a371ff6aa47affc512c56fd21d49365d
holder1-cmyk <positionexchange.holder@gmail.com>
2022-06-01 14:08:32 +0700

    fix: replaceAll

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,7 +1,7 @@
 import { POSITION_EXCHANGE_URL, POSITION_EXCHANGE_REF_CODE } from 'utils/constants'
 
 export const getPostionExchangeUrl = (endpoint: string): string => {
-  let url = `${POSITION_EXCHANGE_URL}/${endpoint.replaceAll('/', '')}`
+  let url = `${POSITION_EXCHANGE_URL}/${endpoint}`
 
   return POSITION_EXCHANGE_REF_CODE
     ? url + `/?ref=${POSITION_EXCHANGE_REF_CODE}`
